6e52ad125fdf37d5bab055525b9a7ebb9fd419dc
jwoodlee <jasonw@crowdtap.com>
2012-08-09 17:32:58 -0400

    now we know

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Locomotion::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-match 'sessions/callback' => "sessions#callback"
+match 'callback' => "sessions#callback"
 match 'brands/:name' => 'brands#show'
 match 'checkin' => 'checkins#create'
 root :to => "sessions#new"
