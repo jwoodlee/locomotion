94fccd827aaade0e3cb40b16594af4a08cc280e1
jwoodlee <jasonw@crowdtap.com>
2012-08-09 19:44:27 -0400

    updating call back

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,10 +1,19 @@
 class CheckinsController < ApplicationController
 
 
-def create
+  def create
 
-  logger.info  "i recieved #{params}"
+    logger.info  "i recieved #{params}"
+    checkin = params[:checkin][:id]
+    response_url = 'http://www.crowdtap.com'
 
-end
+    RestClient.post 'https://api.foursquare.com/v2/checkins/' + checkin + '/reply', 
+      :CHECKIN_ID => checkin, 
+      :oauth_token => session[:access_token],
+      :url => response_url,
+      :text => 'Awesome you checked in go here ' + response_url,
+      :v => '20120801'
+  end
+
 
 end
