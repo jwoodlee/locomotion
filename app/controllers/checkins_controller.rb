69ac0fcac0e31b777ed159773107fbc746169868
jwoodlee <jasonw@crowdtap.com>
2012-08-09 20:45:41 -0400

    tired

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -4,12 +4,17 @@ class CheckinsController < ApplicationController
   def create
 
     checkin = params["checkin"]["id"]
+    logger.info "checkin  #{checkin}"
     userid  = params["user"]["id"]
+    logger.info "userid for checkin #{userid}"
+    user = User.where(:uid => userid).first 
+    logger.info "user is  " + user.to_json
+
     response_url = 'http://www.crowdtap.com'
 
     RestClient.post 'https://api.foursquare.com/v2/checkins/' + checkin + '/reply', 
       :CHECKIN_ID => checkin, 
-      :oauth_token => User.where(:uid => userid).first.access_token ,
+      :oauth_token => user.access_token ,
       :url => response_url,
       :text => 'Awesome you checked in go here ' + response_url,
       :v => '20120801'
