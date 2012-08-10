25bea4436355742cc8e7a7e91c06d30b1ef7f822
Maulin Pathare <mpathare@gmail.com>
2012-08-10 16:27:04 -0400

    added checkin model

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -9,7 +9,7 @@ class CheckinsController < ApplicationController
     venue = checkin["venue"]
     venue_name = venue["name"]
     user = User.where(:uid => user_id).first
-    user.checkin.create(:checkin_id => checkin_id, :venue_name => venue_name)
+    user.checkins.create(:checkin_id => checkin_id, :venue_name => venue_name)
     logger.info "checkin #{checkin}"
     logger.info "user #{user}"
     logger.info "db user is  " + user.to_json
