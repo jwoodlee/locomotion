274aae49e6a89c73b6c950a15fb2a4b905acb5e0
jwoodlee <jasonw@crowdtap.com>
2012-08-10 17:08:31 -0400

    hate sesssion conditions

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -3,7 +3,7 @@ class BrandsController < ApplicationController
 
 def show
   u=current_user
-  logger.info "inside show " + u
+  logger.info "inside show #{u}"
   @brand=Brand.where(:name => params[:name]).first
 end
 
