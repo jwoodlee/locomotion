3947b8e5c2c8d0a88153d2d73f81fff0844a0176
jwoodlee <jasonw@crowdtap.com>
2012-08-10 18:21:27 -0400

    kings of typos

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -9,11 +9,14 @@ class CheckinsController < ApplicationController
     venue = checkin["venue"]
     venue_name = venue["name"]
     user = User.where(:uid => user_id).first
-    user.checkins.create(:checkin_id => checkin_id, :venue_name => venue_name)
-    logger.info "checkin #{checkin}"
-    logger.info "user #{user}"
-    logger.info "db user is  " + user.to_json
-    send_reply_to(checkin_id, user, venue_name)
+    if user.present? 
+      user.checkins.create(:checkin_id => checkin_id, :venue_name => venue_name) 
+      logger.info "checkin #{checkin}"
+      logger.info "user #{user}"
+      logger.info "db user is  " + user.to_json
+      send_reply_to(checkin_id, user, venue_name) 
+    end
+    render :nothing => true , :status => 204
   end
 
   def send_reply_to(checkin_id, user, venue_name)
@@ -21,11 +24,11 @@ class CheckinsController < ApplicationController
     brand = Brand.where(:name => "crowdtap").first unless brand.present?
     response_url = 'http://thawing-headland-3901.herokuapp.com/brands/' + brand.name + "?user_id=#{user.id}"
     RestClient.post 'https://api.foursquare.com/v2/checkins/' + checkin_id + '/reply',
-      :CHECKIN_ID => checkin_id,
-      :oauth_token => user.access_token ,
-      :url => response_url,
-      :text => 'Awesome you checked! Tap here for a chance to win more points.' + response_url,
-      :v => '20120801'
+    :CHECKIN_ID => checkin_id,
+    :oauth_token => user.access_token ,
+    :url => response_url,
+    :text => 'Awesome you checked! Tap here for a chance to win more points.' + response_url,
+    :v => '20120801'
   end
 
   def add_post(checkin_id, user, venue_name)
